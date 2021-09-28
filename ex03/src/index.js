9213b7ade11db598464d592fb753f7716d1cb51e
timurseta <timur_seta@hotmail.com>
2021-09-28 22:27:53 +0200

    static message

---
+++ ex03/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+class Person {
+    static display() {
+        var message = "Static method is invoked from Person class";
+        return message;
+    }
+
+    show(display) {
+        console.log(Person.display());
+    }
+}
+// Only change code above this line
+var message = new Person();
+message.show();
+module.exports = Person;

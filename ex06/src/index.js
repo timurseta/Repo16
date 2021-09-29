c4c2c5ba5be29e83ea9af5cef086135b97df87ce
timurseta <timur_seta@hotmail.com>
2021-09-29 09:06:19 +0200

    function exports

---
+++ ex06/src/index.js
@@ -0,0 +1,9 @@
+function firstName(firstName) {
+    return firstName.toUpperCase();
+}
+function lastName(lastName) {
+    return lastName.toUpperCase();
+}
+
+exports.firstName = firstName;
+exports.lastName = lastName;
